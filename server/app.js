85ef5d1487b093229862f9cdc11a8341b2ae4106
shonubijerry <shonubijerry@gmail.com>
2019-05-30 09:22:47 +0100

    Change API documentation from API Blueprint to Swagger OpenAPI

--- server/app.js
+++ server/app.js
@@ -3,9 +3,12 @@ import Express from 'express';
 import cors from 'cors';
 import debug from 'debug';
 import bodyParser from 'body-parser';
-import path from 'path';
 import dotenv from 'dotenv';
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 import routes from './routes/index';
+import ResponseHelper from './helpers/responseHelper';
+import errorStrings from './helpers/errorStrings';
 
 /**
 * @fileOverview - application entry point
@@ -27,17 +30,29 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({
   extended: false,
 }));
+app.use(bodyParser.json());
 
 // 3rd party middleware
 app.use(cors('*'));
 
-// At the moment GET request on '/' should show documentation inside ./index.html
-app.use('/', Express.static(path.join(__dirname, './public/api-docs')));
-
-app.use(bodyParser.json());
-app.use('/api-docs', Express.static(path.join(__dirname, './public/api-docs')));
-
 routes(app);
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// At the moment GET request on '/' should show documentation
+app.use('/api/v1', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// handles 500 error
+app.use((err, req, res, next) => {
+  if (!err) return next();
+  return res.status(500).json({
+    status: 500,
+    error: 'OOps! Looks like something broke',
+  });
+});
+
+// declare 404 route
+app.all('*', (req, res) => ResponseHelper.error(res, 404, errorStrings.pageNotFound));
 
 // listen to app port
 app.listen(port, debug('app/debug')(`App listening on port ${port}`));
